be198ccdb8aa37ff5afc9541cabb03c1f75b8e24
Jumahl <juanmaanuelhernandez23@gmail.com>
2023-02-17 21:35:15 -0500

    Se crea lo necesario para iniciar con el Footer
    Creo la carpeta componentes, y añado el archivo footer.jsx a la carpeta y añado imagen sin fondo en la capeta de imágenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import image from './img/img.jpeg';
-import Ejemplo from './components/ejemplo/Ejemplo';
+import Footer from './componentes/Footer.jsx';
+
 
 export default function App() {
   return (
-    <><h1 className="text-3xl font-bold underline">
-      {/* components */}
-      Hello world!
-    </h1>
-    <div>
-      <img src={image} alt=""/>
-    </div>
-      <Ejemplo></Ejemplo>
+    <>
+    <Footer/>
     </>
   )
 }
+
+
