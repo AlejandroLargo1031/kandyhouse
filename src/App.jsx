5f68450f3ee3b2060fae57957d235bce811fde42
AlvaroJose1357 <joeibarrafernandez@gmail.com>
2023-02-19 03:02:23 -0500

    insert img and effect(no aparecen)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Servicios from "./components/Servicios.jsx"
+import Servicios from "./components/Servicios.jsx";
 
 export default function App() {
   return (
     <>
-    <Servicios></Servicios>
+    <Servicios/>
     </>
   )
 }
