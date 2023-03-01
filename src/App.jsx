85ca3b99c82b0dbb402de34315040285ce2eafdc
JavierL10 <jroja2043@gmail.com>
2023-02-28 21:09:01 -0500

    pagina de vision

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import image from './img/img.jpeg';
-import Ejemplo from './components/ejemplo/Ejemplo';
+
+import Vision from './components/Vision';
 
 export default function App() {
   return (
     <><h1 className="text-3xl font-bold underline">
       {/* components */}
-      Hello world!
+      
     </h1>
-    <div>
-      <img src={image} alt=""/>
-    </div>
-      <Ejemplo></Ejemplo>
+    
+      <Vision></Vision>
     </>
   )
 }
