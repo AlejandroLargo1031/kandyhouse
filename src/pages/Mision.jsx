8fcbb68f7522e70d9557b48d9e1c1065d014a9f3
CarlosR99 <jose.carlos.silva@correounivalle.edu.co>
2023-03-06 23:25:50 -0500

    actualizacion

--- src/pages/Mision.jsx
+++ src/pages/Mision.jsx
@@ -1,34 +1,28 @@
 import React from "react";
 import logo from "../img/logo-sinfondo.png";
 function Mision() {
-    return (
+  return (
     <>
-    
-    <div className=" place-items-center p-5 bg-rose-300 grid ">
-    <div className= "justify-center bg-white h-50 p-5 rounded-xl">
-        <h1 className="text-4xl text-center font-serif italic ">
-           Mision 
-            
-            
-            
-        </h1>
-
-         <h1 className=" text-center ">
-         <p className="text-3xl leading-8 font-serif italic "> Ofreciendo cada vez un servicio mas completo para nuestros clientes, 
-          satisfaciendo todas sus necesidades en el area, teniendo siempre como  base principal la guia divina de nuestro Dios quien nos otorga valores y virtudes tales como la paciencia, 
-            la dedicacion, la   responsabilidad, la empatia, la honestidad y el amor, con las cuales podremos garantizar un exelente servicio y una complera satisfaccion de nuestros clientes.</p>
-         </h1>
-         <div className="bg-white flex justify-center">
-            <img
-              className="rounded-xl object-center"
-              src={logo}
-            />
+      <div className="h-screen w-full place-items-center p-5 bg-rose-300">
+        <div className="container mx-auto justify-center bg-white p-8 rounded-xl w-auto">
+          <h1 className="text-4xl text-center font-serif italic ">Mision</h1>
+          <div>
+            <h1 className=" text-center text-3xl leading-7 font-serif italic w-full">
+              
+                Ofreciendo cada vez un servicio mas completo para nuestros
+                clientes, satisfaciendo todas sus necesidades en el area,
+                teniendo siempre como base principal la guia divina de nuestro
+                Dios quien nos otorga valores y virtudes tales como la
+                paciencia, la dedicacion, la responsabilidad, la empatia, la
+                honestidad y el amor, con las cuales podremos garantizar un
+                exelente servicio y una complera satisfaccion de nuestros
+                clientes.
+            </h1>
+            <div className="flex p-5 justify-center"><img src={logo} className= "w-52"/></div>
           </div>
-         
-         </div> 
-        
-    </div>
+        </div>
+      </div>
     </>
-    )
+  );
 }
-export default Mision
+export default Mision;
