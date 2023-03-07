b59d368851b636c19b866e22eb2513b606484144
CarlosR99 <jose.carlos.silva@correounivalle.edu.co>
2023-03-06 22:35:56 -0500

    creacion de mision

--- src/pages/Mision.jsx
+++ src/pages/Mision.jsx
@@ -1,11 +1,34 @@
+import React from "react";
+import logo from "../img/logo-sinfondo.png";
 function Mision() {
-  return (
+    return (
     <>
-      <div className="w-full h-screen bg-rose-300">
-        <h1 className="text-white bg-gray-600 p-2">Mision</h1>
-      </div>
-    </>
-  );
-}
+    
+    <div className=" place-items-center p-5 bg-rose-300 grid ">
+    <div className= "justify-center bg-white h-50 p-5 rounded-xl">
+        <h1 className="text-4xl text-center font-serif italic ">
+           Mision 
+            
+            
+            
+        </h1>
 
-export default Mision;
+         <h1 className=" text-center ">
+         <p className="text-3xl leading-8 font-serif italic "> Ofreciendo cada vez un servicio mas completo para nuestros clientes, 
+          satisfaciendo todas sus necesidades en el area, teniendo siempre como  base principal la guia divina de nuestro Dios quien nos otorga valores y virtudes tales como la paciencia, 
+            la dedicacion, la   responsabilidad, la empatia, la honestidad y el amor, con las cuales podremos garantizar un exelente servicio y una complera satisfaccion de nuestros clientes.</p>
+         </h1>
+         <div className="bg-white flex justify-center">
+            <img
+              className="rounded-xl object-center"
+              src={logo}
+            />
+          </div>
+         
+         </div> 
+        
+    </div>
+    </>
+    )
+}
+export default Mision
