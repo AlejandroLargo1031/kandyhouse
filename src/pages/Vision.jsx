cdfcd290ed33cc5e6cdd0326dd57fc3113bbc57c
Javier Lasso <125415549+Jlroja@users.noreply.github.com>
2023-03-14 20:21:28 -0500

    se quita el anterior vision

--- src/pages/Vision.jsx
+++ src/pages/Vision.jsx
@@ -31,21 +31,6 @@ export default function Vision() {
 
     
   
-      <div className="h-screen w-full place-items-center p-5 bg-rose-300 grid ">
-        <div className="justify-center bg-white h-3/6 p-8 rounded-xl">
-          <h1 className="text-4xl text-center">Vision</h1>
-
-          <h1 className="text-center">
-            <p className="text-3xl leading-8  ">
-              {" "}
-              Nos vemos posicionados en el mercado como la casa de eventos mas
-              completa a nivel local y nacional. nos vemos generando empleo a
-              muchas familias y siendo los preferidos de millones, por nuestra
-              calidad de servicio.
-            </p>
-          </h1>
-        </div>
-      </div>
 
     </>
   );
